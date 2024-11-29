46ee7a1035b65c662a5ec0403f0ecb7f684dd373
Silvia Prieto <info@silviaprieto.com>
2024-11-29 10:04:16 +0100

    media queries

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,3 +1,4 @@
+import "../scss/layout/CharacterCard.scss";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
@@ -5,7 +6,7 @@ function CharacterCard({ photo, name, specie, id }) {
   return (
     <li>
       <Link to={`/person/${id}`}>
-        <img src={photo} alt={name} />
+        <img className="photo-list" src={photo} alt={name} />
         <h2>{name}</h2>
         <p>{specie}</p>
       </Link>

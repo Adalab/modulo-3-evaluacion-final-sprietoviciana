e44e58106caffb3414c4dc4eb8f04d32efa5a415
Silvia Prieto <info@silviaprieto.com>
2024-11-29 12:46:33 +0100

    link to character not found

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -13,39 +13,44 @@ function CharacterDetail({ characters }) {
   const character = characters.find((character) => idPerson == character.id);
 
   if (character === undefined) {
-    return <p>El personaje que buscas no existe</p>;
+    return (
+      <Link to={"/"} className="back-list">
+        <p className="not-found">
+          El personaje que buscas no existe, pincha aqui para volver.
+        </p>
+      </Link>
+    );
   }
 
   return (
-<>
-    <Link to={"/"} className="back">
-    {" "}
-    <p className="back-a">Volver</p>
-  </Link>
+    <>
+      <Link to={"/"} className="back">
+        <p className="back-a">Volver</p>
+      </Link>
 
-    <section className="section-character">
-      <img
-        className="section-character-img"
-        src={character.photo}
-        alt={character.name}
-      />
-      <h3 className="title-name">{character.name}</h3>
-      <p>
-        <b>Status: </b>
-        {character.status}
-      </p>
-      <p>
-        <b>Species: </b>
-        {character.species}
-      </p>
-      <p>
-        <b>Origin: </b>
-        {character.origin}
-      </p>
-      <p>
-        <b>Episodes: </b> {character.episodes}
-      </p>
-    </section>
+      <section className="section-character">
+        <img
+          className="section-character-img"
+          src={character.photo}
+          alt={character.name}
+        />
+        <h3 className="title-name">{character.name}</h3>
+        <p>
+          <b>Status: </b>
+          {character.status}
+        </p>
+        <p>
+          <b>Species: </b>
+          {character.species}
+        </p>
+        <p>
+          <b>Origin: </b>
+          {character.origin}
+        </p>
+        <p>
+          <b>Episodes: </b> {character.episodes}
+        </p>
+      </section>
     </>
   );
 }
